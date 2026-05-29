83f2f770f5703c9789cebc99dc7047d671f0ad7e
Shubham Upadhyay <shubhamupadhyay765@gmail.com>
2026-05-30 00:03:21 +0530

    Create 02_instance-vs-class-attributes.py

---
+++ 02_instance-vs-class-attributes.py
@@ -0,0 +1,8 @@
+class employee:
+    language = "python" #this is class attribute
+    salary = 99000
+    
+shubham = employee()
+shubham.language = "javascript" #this is instance attribute
+print(shubham.salary,shubham.language)
+
